cc2f104d9fc8f2aa7e7a348ed83735e3830a6754
Gmall <imalliaras@gmail.com>
2018-11-08 10:51:30 +0300

    Refactored beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,14 +4,16 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-it('should render CommentBox component', () => {
-    const component = shallow(<App />);
+let component;
 
+beforeEach(() => {
+    component = shallow(<App />);
+});
+
+it('should render CommentBox component', () => {
     expect(component.find(CommentBox).length).toEqual(1);
 });
 
 it('should render CommentList component', () => {
-    const component = shallow(<App />);
-
     expect(component.find(CommentList).length).toEqual(1);
 });
